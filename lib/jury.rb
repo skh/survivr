accf91bc30e52d6edfa1f397f78d2b93174b339a
Sonja Krause-Harder <skh@rcane.de>
2016-03-09 21:57:00 +0100

    Finish Jury implementation

--- lib/jury.rb
+++ lib/jury.rb
@@ -8,13 +8,33 @@ class Jury
 		@members << member
 	end
 	def cast_votes(finalists)
-		votes = {}
+		votes = {
+			finalists[0] => 0,
+			finalists[1] => 0
+		}
 		members.each do |member|
-			puts finalists[0]
-		end
-		finalists.each do |finalist|
-			votes[finalist] = 0
+			vote = finalists.sample
+			puts vote
+			votes[vote] += 1
 		end
 		return votes
+	end
+	def report_votes(votes)
+		votes.each do |finalist, score|
+			puts "#{finalist}: #{score}"
+		end
+	end
+	def announce_winner(votes)
+		# I am not too proud of this and would be very interested
+		# in a more elegant solution
+		winner = ""
+		winning_score = 0
+		votes.each do |finalist, score|
+			if score > winning_score
+				winning_score = score
+				winner = finalist
+			end
+		end
+		return winner
 	end
 end
