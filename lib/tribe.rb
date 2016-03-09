a68136b541b59840e6508d0c0f566447676bfd0a
Sonja Krause-Harder <skh@rcane.de>
2016-03-09 22:40:19 +0100

    Implement Tribe

---
+++ lib/tribe.rb
@@ -0,0 +1,16 @@
+class Tribe
+	attr_reader :name, :members
+
+	def initialize(options={})
+		@name = options[:name]
+		@members = options[:members]
+		puts "Tribe #{@name} created."
+	end
+	def to_s
+		@name
+	end
+	def tribal_council(options={})
+		not_immune = @members.select {|member| member != options[:immune]}
+		return not_immune.sample
+	end
+end
